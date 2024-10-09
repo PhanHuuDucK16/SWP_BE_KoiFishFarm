55bdbdf9960414c24b56e33bd9dc7b82815e60bb
PhanHuuDucK16 <phanhuuduc2002tn@gmail.com>
2024-10-09 15:06:41 +0700

    Add repository, Auth with jwt and batch service

--- Application/Mappings/MappingProfile.cs
+++ Application/Mappings/MappingProfile.cs
@@ -3,6 +3,7 @@ using Common.Constants;
 using Domain.Entities;
 using Domain.Models.Authorization;
 using Domain.Models.Creates;
+using Domain.Models.Update;
 using Domain.Models.Views;
 
 namespace Application.Mappings;
@@ -11,9 +12,27 @@ public class MappingProfile: Profile
 {
     public MappingProfile()
     {
+        CreateMap<User, UserInformationModel>();
+        CreateMap<User, UserViewModel>();
+        
+        CreateMap<Role, RoleViewModel>();
+        CreateMap<RoleCreateModel, Role>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()));
+        CreateMap<RoleUpdateModel, Role>();
+
         CreateMap<DeliveryCompany, DeliveryCompanyViewModel>();
         CreateMap<DeliveryCompanyCreateModel, DeliveryCompany>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => DeliveryCompanyStatuses.Active));
+        CreateMap<DeliveryCompanyUpdateModel, DeliveryCompany>();
+        
+        CreateMap<Batch, BatchViewModel>();
+        CreateMap<BatchCreateModel, Batch>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()));
+        CreateMap<BatchUpdateModel, Batch>();
+        
+        CreateMap<FishCreateModel, Fish>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => DeliveryCompanyStatuses.Active));;
     }
 }

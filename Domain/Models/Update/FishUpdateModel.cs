57c67479547c664f11a8d167dd9464584f904522
PhanHuuDucK16 <phanhuuduc2002tn@gmail.com>
2024-10-10 23:05:20 +0700

    api for fish, role

--- Domain/Models/Update/FishUpdateModel.cs
+++ Domain/Models/Update/FishUpdateModel.cs
@@ -2,19 +2,31 @@
 
 namespace Domain.Models.Update;
 
-public class BatchUpdateModel
+public class FishUpdateModel
 {
     public Guid Id { get; set; }
 
     public string Name { get; set; } = null!;
 
+    public string? Description { get; set; }
+
+    public string Origin { get; set; } = null!;
+
+    public int Size { get; set; }
+
     public IFormFile? Thumbnail { get; set; } = null!;
+
+    public DateTime DateOfBirth { get; set; }
 
     public int Price { get; set; }
 
     public int? PromotionPrice { get; set; }
 
     public Guid CreatorId { get; set; }
+
+    public Guid? BatchId { get; set; }
+
+    public string Status { get; set; } = null!;
 
     public DateTime CreateAt { get; set; }
 }
